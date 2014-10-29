8aa18c040ad9747cbbbf9ead46783956f1a4947b
feixufeng <780215342@qq.com>
2014-10-29 17:09:38 +0800

    将播放速度减半

--- HearMeNow/ViewController.m
+++ HearMeNow/ViewController.m
@@ -99,6 +99,10 @@
         if (!error)
         {
             soundPlayer.delegate = self;
+            
+            soundPlayer.enableRate = YES;
+            soundPlayer.rate = 0.5;
+            
             [soundPlayer play];
         } else {
             NSLog(@"Error initializing player: %@", error);
